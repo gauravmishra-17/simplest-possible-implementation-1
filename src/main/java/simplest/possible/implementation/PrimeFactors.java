1a898785f5e9ea156e38d382fc6d7747467292e6
Gaurav Mishra <gaurav@Gauravs-MacBook-Pro.local>
2021-01-04 23:44:14 +0530

    implementation

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public List<Integer> of(int i) {
+        List<Integer> list = new ArrayList<>();
+        return list;
+    }
+}
